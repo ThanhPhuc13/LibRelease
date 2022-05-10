e9b9c5f0a82a1394637d798a409e86c6b17bd9bf
neji <gmail>
2022-05-10 08:39:17 +0700

    test release

--- LibTest1/src/main/java/com/example1/libtest1/Sumga.kt
+++ LibTest1/src/main/java/com/example1/libtest1/Sumga.kt
@@ -3,7 +3,7 @@ package com.example1.libtest1
 class Sumga {
     companion object {
         fun show(): String {
-            return "Sumga chao moi nguoi. jar"
+            return if(BuildConfig.DEBUG) "(Debug)Sumga chao moi nguoi. jar" else "(Release)Sumga chao moi nguoi. jar"
         }
     }
 }
