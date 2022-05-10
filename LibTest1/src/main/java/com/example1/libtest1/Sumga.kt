8380be721074f6209b992599bac5bfa3fed7b552
neji <gmail>
2022-05-10 09:31:34 +0700

    enable minify

--- LibTest1/src/main/java/com/example1/libtest1/Sumga.kt
+++ LibTest1/src/main/java/com/example1/libtest1/Sumga.kt
@@ -1,9 +1,12 @@
 package com.example1.libtest1
 
+import androidx.annotation.Keep
+
 class Sumga {
     companion object {
         fun show(): String {
-            return if(BuildConfig.DEBUG) "(Debug)Sumga chao moi nguoi. jar" else "(Release)Sumga chao moi nguoi. jar"
+            val state = if (BuildConfig.DEBUG) "DEBUG" else "RELEASE"
+            return "(${state})Sumga chao moi nguoi. jar"
         }
     }
 }
---
+++ LibTest1/src/main/java/com/example1/libtest1/SumgaNotEn.kt
@@ -0,0 +1,9 @@
+package com.example1.libtest1
+
+class SumgaNotEn {
+    companion object {
+        fun show(): String {
+            return Sumga.show()
+        }
+    }
+}
